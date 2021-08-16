8327ee9c23826b8a6db1f17be024f9a956e1ff41
ishaanmanu <82237042+ishaanmanu@users.noreply.github.com>
2021-08-16 15:34:30 -0400

    Add files via upload

---
+++ js/Code.js
@@ -0,0 +1,25 @@
+const accessCode1 = "VARIABLE";
+const accessCode2 = "FUNCTION";
+const accessCode3 = "DATABASE";
+
+function clues() {
+    
+    fill("white")
+    textSize(15)
+    text("R E V B A I L A", 100,50)
+    fill("lightblue")
+    text("Hint: Always changing, not constant !!", 100,70)
+
+    fill("white")
+    textSize(15)
+    text("T O C N N U I F", 700,150)
+    fill("lightblue")
+    text("Hint: Performs a Certain Task", 700,170)  
+    
+    fill("white")
+    textSize(15)
+    text("B S A T E D A", 400,250)
+    fill("lightblue")
+    text("Hint: Stores Information", 400,278)    
+
+}
